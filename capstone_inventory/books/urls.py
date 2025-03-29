beeae32385025f7a11af55d1734bf3940fc5afaf
Dakilang-Tamad <107299589+Selwynuy@users.noreply.github.com>
2025-03-29 19:39:09 +0800

    improved UI on return book

--- capstone_inventory/books/urls.py
+++ capstone_inventory/books/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', BookListView.as_view(), name='book-list'),
     path('checkout/<int:book_id>/', checkout_book, name='checkout-book'),
     path('return/<int:transaction_id>/',
-         login_required(return_book), name='return-book'),
+         return_book, name='return-book'),
     path('admin/borrower-dashboard/',
          borrower_dashboard, name='borrower-dashboard'),
 
